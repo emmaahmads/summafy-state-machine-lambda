2a52df700b33f4ab2b7de5e1d313e0a70f170d07
Emma Ahmad <emmaahmadsir@gmail.com>
2024-12-23 23:47:43 +0800

    init

---
+++ roles.tf
@@ -0,0 +1,81 @@
+// Create 2 roles
+// 1. step_function - allows any action on Cloudwatch
+//                  - trust policy - only this AWS service can assume role 
+// 2. cloudwatch_event - 
+
+
+resource "aws_iam_role" "step_function_role" {
+  name = "StepFunctionExecutionRole"
+  
+  // the following is a trust policy
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      /* The `Principal` attribute defines the entity that is allowed to assume the role. The `Service` sub-attribute is set to "states.amazonaws.com", which corresponds to the AWS Step Functions service. This means that only AWS Step Functions can assume this role. In summary, this Terraform resource creates an IAM role with a trust policy that allows AWS Step Functions to assume the role. This setup is essential for enabling Step Functions to execute state machines with the permissions granted to the role, ensuring that the state machines can interact with other AWS resources as needed.*/
+      Principal = {
+        Service = "states.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "step_function_policy" {
+  name        = "StepFunctionPolicy"
+  description = "Policy for Step Function to access resources"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = [
+        "logs:*"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "step_function_policy_attachment" {
+  role       = aws_iam_role.step_function_role.name
+  policy_arn = aws_iam_policy.step_function_policy.arn
+}
+
+resource "aws_iam_role" "cloudwatch_event_role" {
+  name = "CloudWatchEventRole"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "events.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "cloudwatch_event_policy" {
+  name = "CloudWatchEventPolicy"
+  description = "Policy for CloudWatch Events to invoke Step Functions"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "states:StartExecution"
+      ]
+      Resource = [
+        aws_sfn_state_machine.state_machine.arn
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_event_policy_attachment" {
+  role = aws_iam_role.cloudwatch_event_role.name
+  policy_arn = aws_iam_policy.cloudwatch_event_policy.arn
+}
---
+++ statemachine.tf
@@ -0,0 +1,71 @@
+data "aws_s3_bucket" "source_bucket" {
+  bucket = "superumi-summafy-123"
+}
+
+data "aws_cloudformation_stack" "functions" {
+  name = "summafy-lambdas"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = data.aws_s3_bucket.source_bucket.id
+  eventbridge = true
+}
+
+resource "aws_sfn_state_machine" "state_machine" {
+  name     = "PdfProcessingStateMachine"
+  role_arn = aws_iam_role.step_function_role.arn
+
+  definition = jsonencode({
+    Comment = "The state machines of the pdf processing"
+    StartAt = "ReadPdf"
+    States = {
+      ReadPdf = {
+        Type       = "Task"
+        Resource   = data.aws_cloudformation_stack.functions.outputs["SummafyReadS3Function"]
+        Next = "Summarize"
+      },
+      Summarize = {
+        Type       = "Task"
+        Resource   = data.aws_cloudformation_stack.functions.outputs["SummafySummarizeFunction"]
+        Next = "WriteDb"
+      },
+      WriteDb = {
+        Type       = "Task"
+        Resource   = data.aws_cloudformation_stack.functions.outputs["SummafyWriteDbFunction"]
+        End = true
+      }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_rule" "s3_event_rule" {
+  name        = "S3EventRule"
+  description = "Rule to trigger Step Function on S3 create event"
+  event_pattern = jsonencode({
+    source = ["aws.s3"]
+    detail-type = ["Object Created"]
+    detail = {
+      bucket = {
+        name = [data.aws_s3_bucket.source_bucket.bucket]
+      }
+    }
+  })
+}
+
+// specify a target for a cw event
+resource "aws_cloudwatch_event_target" "start_sm" {
+  rule       = aws_cloudwatch_event_rule.s3_event_rule.name
+  arn        = aws_sfn_state_machine.state_machine.arn
+  role_arn   = aws_iam_role.cloudwatch_event_role.arn
+}
+
+# // allows StepFunction to invoke Lambda functions
+
+# resource "aws_lambda_permission" "allow_cloudwatch_to_invoke" {
+#   statement_id  = "AllowExecutionFromCloudWatch"
+#   action        = "states:StartExecution"
+#   function_name = aws_sfn_state_machine.state_machine.name
+#   principal     = "events.amazonaws.com"
+#   source_arn    = aws_cloudwatch_event_rule.s3_event_rule.arn
+# }
+
